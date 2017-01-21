4415d16db4846e31d95ae8143f92f24f4a17766c
marshallcannon <marshcannon@gmail.com>
2017-01-20 22:50:47 -0700

    Initial Setup

---
+++ main.js
@@ -0,0 +1,7 @@
+//Create Game
+var screen1 = new Phaser.Game(960, 540, Phaser.CANVAS, 'game', loadingState);
+var screen2 = new Phaser.Game(960, 540, Phaser.CANVAS, 'game', loadingState);
+
+//Controllers
+var pad1;
+var pad2;
